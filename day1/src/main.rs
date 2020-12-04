9fced69a742ca7110abff704d9f80532af8e0ee5
Niklas Borson <NIKLASB@microsoft.com>
2020-12-04 10:49:16 -0800

    Improve error handling.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,16 +4,27 @@ use std::io::{prelude::*, BufReader};
 fn main() {
 
     // Read the file into a vector of i32.
-    let mut v = read_numbers("day1-input.txt");
+    match read_numbers("day1-input.txt") {
+        Ok(mut v) => {
 
-    // Sort the numbers, so we can binary_search later.
-    v.sort();
+            // Sort the numbers, so we can binary_search later.
+            v.sort();
 
-    // Find the first pair that sums to 2020.
-    find_pair(&v[..]);
+            // Find the first pair that sums to 2020.
+            if let Some((a, b)) = find_pair(&v[..]) {
+                println!("{} * {} = {}", a, b, a * b);
+            }
+            else {
+                println!("No pair found that sums to 2020.");
+            }
+        },
+        Err(e) => {
+            println!("Error {} reading input file.", e);
+        }
+    }
 }
 
-fn find_pair(v: &[i32]) {
+fn find_pair(v: &[i32]) -> Option<(i32, i32)> {
     for i in 1usize..v.len() {
 
         // Let a be the value before the slice starting at i.
@@ -22,33 +33,36 @@ fn find_pair(v: &[i32]) {
         // Let b the other member of the pair such that a + b == 2020.
         let b = 2020 - a;
 
-        // If be exists then we're done.
-        if let Ok(_i) = v[i..].binary_search(&b) {
-            println!("{} * {} = {}", a, b, a * b);
+        if b < a {
+            // Be can't possibly exist for values >= a.
             break;
         }
-    }
-}
 
-fn read_numbers(path: &str) -> Vec::<i32> {
-    let mut v = Vec::<i32>::new();
-
-    if let Ok(file) = fs::File::open("day1-input.txt") {
-        let reader = BufReader::new(file);
-        for line in reader.lines() {
-            if let Ok(s) = line {
-                if let Ok(n) = s.parse::<i32>() {
-                    v.push(n);
-                }
-                else {
-                    println!("{} is not a number.", s);
-                }
-            }
+        // If be exists then we're done.
+        if let Ok(_i) = v[i..].binary_search(&b) {
+            return Some((a, b));
         }
     }
-    else {
-        println!("Error opening {}", path);
-    }
+    None
+}
 
-    v
+fn read_numbers(path: &str) -> std::io::Result<Vec::<i32>> {
+    match fs::File::open(path) {
+        Ok(file) => {
+            let mut v = Vec::<i32>::new();
+            let reader = BufReader::new(file);
+            for line in reader.lines() {
+                match line {
+                    Ok(s) => {
+                        if let Ok(n) = s.parse::<i32>() { v.push(n); }
+                    },
+                    Err(e) => {
+                        return Err(e)
+                    }
+                }                
+            }
+            Ok(v)
+        },
+        Err(e) => Err(e)
+    }
 }
