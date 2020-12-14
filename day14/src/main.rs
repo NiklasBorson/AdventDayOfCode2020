e1f07b2f4574d663be747f733f4222cfacf210a5
Niklas Borson <NIKLASB@microsoft.com>
2020-12-13 21:51:39 -0800

    Add day 14 part 1.

---
+++ day14/src/main.rs
@@ -0,0 +1,123 @@
+use std::fs;
+use std::io::{prelude::*, BufReader};
+use std::collections::HashMap;
+
+fn main() -> std::io::Result<()> {
+    let code = read_file("input.txt")?;
+
+    part1(&code);
+
+    Ok(())
+}
+
+fn part1(code : &[Instruction]) {
+    let mut computer = Computer::new();
+
+    for inst in code {
+        computer.exec(inst);
+    }
+
+    let mut sum = 0;
+    for (_address, value) in computer.memory {
+        sum += value;
+    }
+
+    println!("Sum of memory = {}", sum);
+}
+
+enum Instruction {
+    Mask {
+        and : u64,
+        or : u64
+    },
+    Mem {
+        address : u64,
+        value : u64
+    }
+}
+
+struct Computer {
+    mask_and : u64,
+    mask_or : u64,
+    memory : HashMap<u64, u64>
+}
+
+impl Computer {
+    fn new() -> Computer {
+        Computer{
+            mask_and : u64::MAX,
+            mask_or : 0,
+            memory : HashMap::new()
+        }
+    }
+
+    fn exec(&mut self, inst : &Instruction) {
+        match inst {
+            Instruction::Mask{ and, or } => {
+                self.mask_and = *and;
+                self.mask_or = *or;
+            },
+            Instruction::Mem{ address, value} => {
+                let val = (*value & self.mask_and) | self.mask_or;
+                self.memory.insert(*address, val);
+            }
+        }
+    }
+}
+
+fn parse_instruction(line : &str) -> Option<Instruction> {
+    const MASK : &str = "mask = ";
+    const MEM1 : &str = "mem[";
+    const MEM2 : &str = "] = ";
+
+    if line.len() >= MASK.len() && &line[..MASK.len()] == MASK {
+        let mut and = 0;
+        let mut or = 0;
+        for ch in line[MASK.len()..].chars() {
+            match ch {
+                'X' => {
+                    and <<= 1;
+                    or <<= 1;
+                    and |= 1;   // keep the input bit
+                },
+                '0' => {
+                    and <<= 1;
+                    or <<= 1;
+                },
+                '1' => {
+                    and <<= 1;
+                    or <<= 1;
+                    or |= 1;
+                },
+                _ => {
+                    println!("Invalid mask: {}", line);
+                    return None;
+                }
+            }
+        }
+        return Some(Instruction::Mask{ and, or });
+    }
+    else if line.len() >= MEM1.len() && &line[..MEM1.len()] == MEM1 {
+        if let Some(i) = line.find(MEM2) {
+            let address_field = &line[MEM1.len()..i];
+            let value_field = &line[i + MEM2.len()..];
+            if let Ok(address) = address_field.parse() {
+                if let Ok(value) = value_field.parse() {
+                    return Some(Instruction::Mem{ address, value });
+                }
+            }
+        }
+    }
+    println!("Invalid instruction: {}", line);
+    None
+}
+
+fn read_file(path: &str) -> std::io::Result<Vec::<Instruction>> {
+    let mut v = Vec::new();
+    for line in BufReader::new(fs::File::open(path)?).lines() {
+        if let Some(inst) = parse_instruction(&line?) {
+            v.push(inst);
+        }
+    }
+    Ok(v)
+}
