cc5c0440df3b0a3db5c0e899a4a685787a94a1e1
Niklas Borson <NIKLASB@microsoft.com>
2020-12-16 22:06:58 -0800

    Solve day 17 part 2.

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,12 +1,21 @@
 use std::fs;
 use std::io::{prelude::*, BufReader};
 
+const MAX_FRAME_COUNT : usize = 6;
+
 fn main() -> std::io::Result<()> {
-    const MAX_FRAME_COUNT : usize = 6;
 
     // Read the initial cells.
     let (width, cells) = read_file("input.txt")?;
     let height = cells.len() / width;
+
+    part1(width, height, &cells);
+    part2(width, height, &cells);
+
+    Ok(())
+}
+
+fn part1(width : usize, height : usize, cells : &[bool]) {
 
     // Allocate a 3d grid with enough margin around it that we won't
     // reach its boundaries in MAX_FRAME_COUNT frames.
@@ -20,7 +29,7 @@ fn main() -> std::io::Result<()> {
     // Set the initial active cells in the grid.
     let mut x = 0;
     let mut y = 0;
-    for cell in cells {
+    for &cell in cells {
         if cell {
             grid.set(x + margin, y + margin, margin, true);
         }
@@ -40,8 +49,43 @@ fn main() -> std::io::Result<()> {
 
     // Output the final number of active cells.
     println!("{} active cells.", grid.count_active_cells());
+}
 
-    Ok(())
+fn part2(width : usize, height : usize, cells : &[bool]) {
+    
+    // Allocate a 4d grid with enough margin around it that we won't
+    // reach its boundaries in MAX_FRAME_COUNT frames.
+    let margin = MAX_FRAME_COUNT + 1;
+    let mut grid = Grid4::new(
+        width + margin * 2,
+        height + margin * 2,
+        1 + margin * 2,
+        1 + margin * 2
+    );
+
+    // Set the initial active cells in the grid.
+    let mut x = 0;
+    let mut y = 0;
+    for &cell in cells {
+        if cell {
+            grid.set(x + margin, y + margin, margin, margin, true);
+        }
+
+        // Advance (x,y) coordinate.
+        x += 1;
+        if x == width {
+            x = 0;
+            y += 1;
+        }
+    }
+
+    // Execute the cellular automaton.
+    for _i in 0..MAX_FRAME_COUNT {
+        grid.next_frame();
+    }
+
+    // Output the final number of active cells.
+    println!("{} active cells.", grid.count_active_cells());
 }
 
 struct Grid3 {
@@ -97,6 +141,79 @@ impl Grid3 {
             for y in y-1..y+2 {
                 for x in x-1..x+2 {
                     count += self.test(x, y, z) as usize;
+                }
+            }
+        }
+        count
+    }
+
+    fn count_active_cells(&self) -> usize {
+        let mut count = 0;
+        for &cell in &self.cells {
+            count += cell as usize;
+        }
+        count
+    }
+}
+
+struct Grid4 {
+    cells : Vec<bool>,
+    next_cells : Vec<bool>,
+    width : usize,
+    height : usize,
+    depth : usize,
+    wlen : usize
+}
+
+impl Grid4 {
+    fn new(width : usize, height : usize, depth : usize, wlen : usize) -> Grid4 {
+        let mut cells = Vec::new();
+        cells.resize(width * height * depth * wlen, false);
+        let next_cells = cells.clone();
+        Grid4{ cells, next_cells, width, height, depth, wlen }
+    }
+
+    fn index(&self, x : usize, y : usize, z : usize, w : usize) -> usize {
+        ((w * self.wlen + z) * self.height + y) * self.width + x
+    }
+
+    fn set(&mut self, x : usize, y : usize, z : usize, w : usize, value : bool) {
+        let i = self.index(x, y, z, w);
+        self.cells[i] = value;
+    }
+
+    fn test(&self, x : usize, y: usize, z : usize, w : usize) -> bool {
+        let i = self.index(x, y, z, w);
+        self.cells[i]
+    }
+
+    fn next_frame(&mut self) {
+        for w in 1..self.wlen - 1 {
+            for z in 1..self.depth - 1 {
+                for y in 1..self.height - 1 {
+                    for x in 1..self.width - 1 {
+                        let i = self.index(x, y, z, w);
+                        let is_active = self.cells[i];
+                        let count = self.count_neighbors_and_self(x, y, z, w) - (is_active as usize);
+                        self.next_cells[i] = match is_active {
+                            true => count == 2 || count == 3,
+                            false => count == 3
+                        };
+                    }
+                }
+            }
+        }
+        std::mem::swap(&mut self.cells, &mut self.next_cells);
+    }
+
+    fn count_neighbors_and_self(&self, x : usize, y : usize, z : usize, w : usize) -> usize {
+        let mut count = 0;
+        for w in w-1..w+2 {
+            for z in z-1..z+2 {
+                for y in y-1..y+2 {
+                    for x in x-1..x+2 {
+                        count += self.test(x, y, z, w) as usize;
+                    }
                 }
             }
         }
