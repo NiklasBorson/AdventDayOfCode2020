4bbded42288459d24b5e3922be26012a9a05267a
Niklas Borson <NIKLASB@microsoft.com>
2020-12-18 19:46:23 -0800

    Solve day 18 part 1.

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -2,24 +2,52 @@ use std::fs;
 use std::io::{prelude::*, BufReader};
 
 fn main() -> std::io::Result<()> {
-    let mut _input = read_file("input.txt");
+    let input = read_file("input.txt")?;
 
-    evaluate("9 + 3 * (9 * (9 + 3 + 5 * 3) * 5 * 3 * 5)");
+    let mut total = 0;
+
+    for line in &input {
+        if let Some(n) = eval(&line) {
+            total += n;
+        }
+        else {
+            println!("Error evaluating expression: {}", &line);
+            break;
+        }
+    }
+
+    println!("total = {}", total);
 
     Ok(())
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq)]
 enum OpType { Add, Mul }
 
-#[derive(Clone, Copy)]
+use OpType::*;
+
+impl OpType {
+    fn eval(&self, lhs : u64, rhs : u64) -> u64 {
+        match self {
+            Add => lhs + rhs,
+            Mul => lhs * rhs
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq)]
 enum Token {
     None,
-    Op (OpType),
-    Number (u32),
+    Operator (OpType),
+    Number (u64),
     Open,
     Close
 }
+
+use Token::Operator;
+use Token::Number;
+use Token::Open;
+use Token::Close;
 
 struct Stack {
     stack : Vec<Token>
@@ -30,66 +58,102 @@ impl Stack {
         Stack{ stack : Vec::new() }
     }
 
-    fn top(&self) -> Token {
-        if let Some(tok) = self.stack.last() {
-            *tok
+    fn frame(&self, index : usize) -> Token {
+        let c = self.stack.len();
+        if index < c {
+            self.stack[c - 1 - index]
         }
         else {
             Token::None
         }
     }
 
+    fn top(&self) -> Token {
+        self.frame(0)
+    }
+
+    fn result(&self) -> Option<u64> {
+        if self.stack.len() == 1 {
+            if let Number(n) = self.stack[0] {
+                return Some(n);
+            }
+        }
+        None
+    }
+
     fn add_token(&mut self, tok : Token) -> Option<()> {
         match tok {
-            Token::None => None,
-            Token::Op(_op) => {
-                if let Token::Number(_value) = self.top() {
-                    self.stack.push(tok);
-                    // TODO
-                    Some(())
-                }
-                else {
-                    None
-                }
+            Token::None =>
+                None,
+            Operator(_op) => {
+                self.stack.push(tok);
+                Some(())
             },
-            Token::Number(_value) => {
-                // TODO
+            Number(value) => {
+                let value = self.reduce(value)?;
+                self.stack.push(Token::Number(value));
+                Some(())
+            }
+            Open => {
                 self.stack.push(tok);
                 Some(())
             }
-            Token::Open => {
-                // TODO
-                self.stack.push(tok);
-                Some(())
-            }
-            Token::Close => {
-                // TODO
+            Close => {
+                let value = self.close_group()?;
+                let value = self.reduce(value)?;
+                self.stack.push(Token::Number(value));
                 Some(())
             }
         }
     }
+
+    fn close_group(&mut self) -> Option<u64> {
+        if let Number(value) = self.top() {
+            self.stack.pop();
+            if self.top() == Open {
+                self.stack.pop();
+                return Some(value);
+            }
+        }
+        None
+    }
+
+    fn reduce(&mut self, value : u64) -> Option<u64> {
+        let mut rhs = value;
+        while let Operator(op) = self.top() {
+            self.stack.pop();
+            if let Number(lhs) = self.top() {
+                rhs = op.eval(lhs, rhs);
+                self.stack.pop();
+            }
+            else {
+                return None;
+            }
+        }
+        Some(rhs)
+    }
 }
 
-fn evaluate(expr : &str) -> Option<u32> {
+fn eval(expr : &str) -> Option<u64> {
 
     //let mut iter = expr.chars();
 
     //      9 + 3 * (9 * (9 + 3 + 5 * 3) * 5 * 3 * 5)
     //
     //      "9"  push Number                   9
-    //      "+"  push Op                    9 +
+    //      "+"  push Operator                    9 +
     //      "3"  pop [+ 9], push 12         12
     //      "*"  push Mul                   12 * 
     //      "("  push Group                 12 * G
     //      "9"  push Number                   12 * G 9
-    //      "*"  push Op                    12 * G 9 *
+    //      "*"  push Operator                    12 * G 9 *
     //      "("  push Group                 12 * G 9 * G
     //      "9"  push Number                   12 * G 9 * G 9
-    //      "+"  push Op                    12 * G 9 * G 9 +
+    //      "+"  push Operator                    12 * G 9 * G 9 +
     //      "3"  pop [+ 9], push 12         12 * G 9 * G 12
-    //      "+"  push Op                    12 * G 9 * G 12 +
+    //      "+"  push Operator                    12 * G 9 * G 12 +
     //      "5"  pop [+ 12], push 17        12 * G 9 * G 17
-    //      "*"  push Op                    12 * G 9 * G 17 *
+    //      "*"  push Operator                    12 * G 9 * G 17 *
     //      "3"  pop [* 17], push 51        12 * G 9 * G 51
     //      ")"  pop 
     //      "*"
@@ -104,14 +168,14 @@ fn evaluate(expr : &str) -> Option<u32> {
 
     for ch in expr.chars() {
         match ch {
-            '+' => { stack.add_token(Token::Op(OpType::Add))?; },
-            '*' => { stack.add_token(Token::Op(OpType::Mul))?; },
-            '(' => { stack.add_token(Token::Open)?; },
-            ')' => { stack.add_token(Token::Close)?; },
+            '+' => { stack.add_token(Operator(OpType::Add))?; },
+            '*' => { stack.add_token(Operator(OpType::Mul))?; },
+            '(' => { stack.add_token(Open)?; },
+            ')' => { stack.add_token(Close)?; },
             ' ' => {},
             _ => {
                 if ch >= '0' && ch <= '9' {
-                    stack.add_token(Token::Number(ch as u32 - '0' as u32))?;
+                    stack.add_token(Number(ch as u64 - '0' as u64))?;
                 }
                 else {
                     return None;
@@ -120,12 +184,7 @@ fn evaluate(expr : &str) -> Option<u32> {
         }
     }
 
-    if let Token::Number(value) = stack.top() {
-        Some(value)
-    }
-    else {
-        None
-    }
+    stack.result()
 }
 
 fn read_file(path: &str) -> std::io::Result<Vec::<String>> {
